1a30f1d4f2f2b079718572264970440853b0ed8b
travisrego <ms.anitarego@gmail.com>
2025-04-14 14:30:12 +0300

    courseinfo completed

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -5,9 +5,17 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <p>{props.name} {props.exercises}</p>
+  )
+}
+
 const Content = (props) => {
   return (
-    <p>{props.content[props.number].name} &nbsp;&nbsp;&nbsp; {props.content[props.number].exercises}</p>
+    <div>
+      <Part name={props.content[props.number].name} exercises={props.content[props.number].exercises} />
+    </div>
   )
 }
 
